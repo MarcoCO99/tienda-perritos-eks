749d3188959ceae875979604fc10a91941c149ff
sanosysalvos <marc.contrerasa@duocuc.cl>
2026-06-28 20:34:10 -0400

    comentarios codigo base

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 
 const {
   DB_HOST = tienda-db, // acá resuelve internamente en eks
-  DB_USER = "root",
+  DB_USER = "root", // Comentario para la prueba de workflows
   DB_PASSWORD = "admin123",
   DB_NAME = "tienda_perritos",
   DB_PORT = 3306,
